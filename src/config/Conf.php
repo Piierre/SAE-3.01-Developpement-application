ecdbf726af5c35fd9342073ca733d5931e970bfd
LAM-Harry <harrylam317@gmail.com>
2025-01-21 00:12:29 +0100

    je dors stp

--- src/config/Conf.php
+++ src/config/Conf.php
@@ -14,7 +14,7 @@
         // Sur votre machine, vous avez surement un compte 'root'
         'login' => 'root',
         // Sur votre machine, vous avez créé ou non ce mdp a l'installation
-        'password' => '123'
+        'password' => ''
         );
         
         static public function getHostname() : string {
