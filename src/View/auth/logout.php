58864d503051ab429b7cf720d7850b3de001e57c
Piierre <wantiezpierreemail@gmail.com>
2025-01-23 15:47:42 +0100

    MAJ

--- src/View/auth/logout.php
+++ src/View/auth/logout.php
@@ -5,7 +5,7 @@ header("Location: login.php");
 exit();
 ?>
 <!DOCTYPE html>
-<html lang="fr"></html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
