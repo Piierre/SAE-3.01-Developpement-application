1381b30a4b0304425ef9989bb9d165e59e4e494b
Adaaaam94 <wiiplayzzhd@gmail.com>
2025-01-24 15:53:54 +0100

    m

--- src/View/home/index.php
+++ src/View/home/index.php
@@ -19,15 +19,14 @@ session_start();
             <ul>
                 <li><a href="#welcome">🏠 Accueil</a></li>
                 <li><a href="#features">⚙️ Fonctionnalités</a></li>
-                <li><a href="../web/frontController.php?page=carte">🗺️ Carte Interactive</a></li>
-                <li><a href="../web/frontController.php?page=recherche">🔍 Recherche</a></li>
                 <?php if (isset($_SESSION['login'])): ?>
                     <li><a href="../web/frontController.php?page=logout">🚪 Déconnexion (<?= htmlspecialchars($_SESSION['login']) ?>)</a></li>
                     <?php if ($_SESSION['role'] === 'admin'): ?>
                         <li><a href="../web/frontController.php?page=manage_users">Gérer les utilisateurs</a></li>
                     <?php endif; ?>
+                <?php else: ?>
+                    <li><a href="../web/frontController.php?page=login">🔑 Connexion</a></li>
                 <?php endif; ?>
-                <li><a href="../web/frontController.php?page=login" class="login-button">🔑 Connexion</a></li>
             </ul>
         </nav>
         <button class="toggle-dark-mode" onclick="toggleDarkMode()">🌙 Mode sombre </button>
