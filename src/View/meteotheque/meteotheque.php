77bce6f96a9318005e322823ac299793b76903a4
Adaaaam94 <wiiplayzzhd@gmail.com>
2025-01-27 00:42:49 +0100

    suppr données inutiles

--- src/View/meteotheque/meteotheque.php
+++ src/View/meteotheque/meteotheque.php
@@ -154,10 +154,7 @@ function genererCouleurAleatoire() {
                         <?php $couleur = genererCouleurAleatoire(); ?>
                         <div class="cube" style="background-color: <?= $couleur ?>;">
                             <strong>Météothèque : <?= htmlspecialchars($meteotheque['titre']) ?></strong>
-                            <p>Description : <?= htmlspecialchars($meteotheque['description']) ?></p>
-                            <p>Station : <?= htmlspecialchars($meteotheque['station_name']) ?></p>
                             <p>Date de recherche : <?= htmlspecialchars($meteotheque['search_date']) ?></p>
-
                             <?php if (!empty($meteotheque['station_name']) && !empty($meteotheque['search_date'])): ?>
                                 <a href="/SAE-3.01-Developpement-application/src/View/map/search.php?station_name=<?= urlencode($meteotheque['station_name']) ?>&date=<?= urlencode($meteotheque['search_date']) ?>&redirect=true">🔍 Rechercher cette station</a>
                             <?php else: ?>
